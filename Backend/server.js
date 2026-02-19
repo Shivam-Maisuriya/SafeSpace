3c150c062c56e3f55e325c36a47cbaad64654c8e
Shivam Maisuriya <maisuriyas89@gmail.com>
2026-02-19 16:45:23 +0530

    add morgan for logging, implement async error handling, and enhance rate limiting

--- Backend/server.js
+++ Backend/server.js
@@ -3,8 +3,11 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import rateLimit from "express-rate-limit";
+import morgan from "morgan";
 
+// Methods
 import connectDB from "./config/db.js";
+import errorHandler from "./middleware/errorHandler.js";
 
 // routes
 import authRoutes from "./routes/authRoutes.js";
@@ -17,29 +20,48 @@ dotenv.config();
 
 const app = express();
 
+app.set("trust proxy", 1);
+
 // Database connection
 connectDB(); 
 
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // limit each IP to 100 requests per window
-  message: "Too many requests, please try again later."
+// 
+const strictLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 20, // 20 requests per 15 min
+  message: "Too many attempts. Please try later."
 });
 
+const mediumLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100
+});
+
+const adminLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 10
+});
+
+
 app.use(cors());
+app.use(morgan("dev"));
 app.use(express.json());
-app.use(limiter);
 
 app.get("/", (req, res) => {
   res.send("SafeSpace API running...");
 });
 
 // routes
-app.use("/api/auth", authRoutes);
-app.use("/api/posts", postRoutes);
-app.use("/api/comments", commentRoutes);
-app.use("/api/reports", reportRoutes);
-app.use("/api/admin", adminRoutes);
+app.use("/api/auth", strictLimiter, authRoutes);
+app.use("/api/reports", strictLimiter, reportRoutes);
+
+app.use("/api/admin", adminLimiter, adminRoutes);
+
+app.use("/api/posts", mediumLimiter, postRoutes);
+app.use("/api/comments", mediumLimiter, commentRoutes);
+
+// centralized error handler (MUST BE LAST)
+app.use(errorHandler);
 
 app.listen(5000, () => console.log("🚀 Server running on port 5000"));
 
